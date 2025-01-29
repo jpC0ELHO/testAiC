bf71a61481108eacb0fc2ff2a47901c4eaf18afc
jpc.human() <110858907+jpC0ELHO@users.noreply.github.com>
2025-01-29 13:44:17 -0300

    modified:   garbagecollection.cpp 	modified:   perceptron.cpp

---
+++ perceptron.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<math.h>
+#include<stdio.h>
+#include<vector>
+
+using namespace std;
+
+
+double __initial(double self, double input_size,double learning_rate, double epochs){
+    
+    
+
+}
